51fab1c8cf9c36dc55cb20248b1a292c7efe694a
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-07-03 19:48:04 -0500

    Evento a un clic y primera función

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Juego del número secreto';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un número del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('Click desde el botón');
+}
