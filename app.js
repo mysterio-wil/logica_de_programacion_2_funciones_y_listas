384ddb4b22968060b79c5f8287e1e6236ca71da6
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-07-04 05:14:52 -0500

    generar un numero aleatorio

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 function asigarTextoElemento (elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML =texto;
+    return;
 }
 
 function intentoDeUsuario() {
     alert('Click desde el botón');
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10) + 1;
 }
 
 asigarTextoElemento('h1', 'Juego del número secreto');
